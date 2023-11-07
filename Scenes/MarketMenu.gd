b14956424bba7ac0ae8aa307ffe87e529555ca6e
unknown <dwlee0802@gmail.com>
2023-11-07 16:34:56 +0900

    Added dropdown menu for market tab to select which good type to show info for. Also added colors to menu buttons for visibility.

--- Scenes/MarketMenu.gd
+++ Scenes/MarketMenu.gd
@@ -4,11 +4,21 @@ var game
 var menuBar
 var menuPanel
 
+var crystalMarketTab
+var steelMarketTab
+var oilMarketTab
+var semiconductorMarketTab
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	menuBar = get_parent()
 	game = menuBar.get_parent().get_parent().get_parent()
 	menuPanel = $MenuPanel
+	crystalMarketTab = menuPanel.get_node("CrystalMarket")
+	steelMarketTab = menuPanel.get_node("SteelMarket")
+	oilMarketTab = menuPanel.get_node("OilMarket")
+	semiconductorMarketTab = menuPanel.get_node("SemiconductorMarket")
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,3 +32,20 @@ func _pressed():
 	
 	if prev == false:
 		menuPanel.visible = true
+
+
+func _on_option_button_item_selected(index):
+	crystalMarketTab.visible = false
+	steelMarketTab.visible = false
+	oilMarketTab.visible = false
+	semiconductorMarketTab.visible = false
+	
+	if index == 0:
+		crystalMarketTab.visible = true
+	if index == 1:
+		steelMarketTab.visible = true
+	if index == 2:
+		oilMarketTab.visible = true
+	if index == 3:
+		semiconductorMarketTab.visible = true
+		

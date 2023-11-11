a042dde0f5a9bb84d9baa34e0055915eb7e7b19c
unknown <dwlee0802@gmail.com>
2023-11-11 19:38:24 +0900

    Improved tutorial page to show controls. Added shortcuts to menu buttons.

--- Scenes/TutorialPage.gd
+++ Scenes/TutorialPage.gd
@@ -1,5 +1,21 @@
-extends Button
+extends Panel
 
+var intro
+var controls
 
-func _on_pressed():
+func _ready():
+	intro = $IntroductionLabel
+	controls = $ControlsLabel
+
+func _on_dismiss_button_pressed():
 	queue_free()
+
+
+func _on_option_button_pressed(extra_arg_0):
+	intro.visible = false
+	controls.visible = false
+	
+	if extra_arg_0 == 0:
+		intro.visible = true
+	if extra_arg_0 == 1:
+		controls.visible = true
